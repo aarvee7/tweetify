6a7a6901a2524eefa094dd2dc8a63287f453fb22
Rajavel Kamaraj <jkamarr@jci.com>
2022-02-09 00:28:49 +0530

    commit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+const config = require('./config/config.json')
+var axios = require('axios').default;
+
+var token;
+axios.defaults.headers = {Authorization : 'Bearer '+ token}
+axios.defaults.baseURL = config.base;
+
+//console.log(axios);
+class main{
+    constructor(key){
+this.key = key;
+    }
+ 
+async getUserByName(name){
+    return await axios.get("/users/by/username/"+name);
+
+    }
+
+    async getUserById(id){
+        return await axios.get("/users/"+id);
+    }
+
+    async getUserTweetsByName(name){
+        const resp = await this.getUserByName(name);       
+        const {id} = resp.data.data;
+        console.log(id);
+        return await axios.get("/users/"+id+"/tweets");
+    }
+    async getUserFollowersByName(name){
+        const resp = await this.getUserByName(name);       
+        const {id} = resp.data.data;
+        console.log(id);
+        return await axios.get("/users/"+id+"/followers");
+    }
+    async getUserBlockedByName(name){
+        const resp = await this.getUserByName(name);       
+        const {id} = resp.data.data;
+        console.log(id);
+        return await axios.get("/users/"+id+"/blocking");
+    }
+
+    async createTweet(text){
+        return await axios.post("/tweets",{text : text });
+    }
+
+
+}
+
+module.exports = {token : token,load : main};
+
+// const newa = new main();
+// newa.createTweet('hellow').then(res => console.log(res.data))
+
+
+
